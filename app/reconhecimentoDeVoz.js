97301babeca360bf53540f79a7a2ee07ee349b1b
Matheus Malena <matheusmalena28@gmail.com>
2024-02-06 18:02:09 -0300

    update

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start();
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(s) {
+    chute = s.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
+}
